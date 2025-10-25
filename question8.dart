d15471f07294c58f94702e63d6658ba2731e1b28
junaidyousuf177 <junaidyousuf177@gmail.com>
2025-10-25 10:01:11 +0500

    Question 8

---
+++ question8.dart
@@ -0,0 +1,11 @@
+void main (){
+    int lenght = 40;
+    int breadth = 50;
+    if (lenght == breadth){
+        print("it is a square");
+        }
+    else{
+        print ("it is a rectangle");
+    }
+    
+}
